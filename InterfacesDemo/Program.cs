df997c37c5551ee7253db32fa0d157f1b32a1723
Mustafa Palta <Mustafapalta00@hotmail.com>
2023-04-02 16:26:00 +0300

    Finish

--- InterfacesDemo/Program.cs
+++ InterfacesDemo/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InterfacesDemo
 {
@@ -10,6 +6,88 @@ namespace InterfacesDemo
     {
         static void Main(string[] args)
         {
+            IWorker[] workers = new IWorker[3]
+            {
+                new Manager(),
+                new Worker(),
+                new Robot()
+            };
+
+            foreach (var worker in workers)
+            {
+                worker.Work();
+            }
+
+            IEat[] eats = new IEat[2]
+            {
+                new Worker(),
+                new Manager()
+            };
+
+            foreach (var eat in eats)
+            {
+                eat.Eat();
+            }
+
+            Console.ReadLine();
+        }
+
+        interface IWorker
+        {
+            void Work();
+        }
+
+        interface IEat
+        {
+            void Eat();
+        }
+
+        interface ISalary
+        {
+            void GetSalary();
+        }
+        class Manager : IWorker, IEat, ISalary
+        {
+            public void Eat()
+            {
+                Console.WriteLine("Şuan Yöneticiler Yemek Yiyor");
+            }
+
+            public void GetSalary()
+            {
+                Console.WriteLine("Şuan Yöneticiler Maaş Alıyor");
+            }
+
+            public void Work()
+            {
+                Console.WriteLine("Şuan Yöneticiler Çalışıyor");
+            }
+        }
+
+        class Worker : IWorker, IEat, ISalary
+        {
+            public void Eat()
+            {
+                Console.WriteLine("Şuan Çalışanlar Yemek Yiyor");
+            }
+
+            public void GetSalary()
+            {
+                Console.WriteLine("Şuan Çalışanlar Maaş Alıyor");
+            }
+
+            public void Work()
+            {
+                Console.WriteLine("Şuan Çalışanlar Çalışıyor");
+            }
+        }
+
+        class Robot : IWorker
+        {
+            public void Work()
+            {
+                Console.WriteLine("Şuan Robotlar Çalışıyor");
+            }
         }
     }
 }
